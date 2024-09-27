a58a525b0422cc24749a3d19593fddefa685326a
goriiin <afk00.17@bk.ru>
2024-09-27 18:55:50 +0300

    [improve] пример использования

--- public/index.js
+++ public/index.js
@@ -1,52 +1,33 @@
 import {buildBody} from "../src/scripts/layouts/body.js";
 import {buildCards} from "../src/scripts/components/card.js";
-import {buildModalWithContent} from "../src/scripts/components/modal-auth/modal-auth.js";
-import {modalSignIn, modalSignUp} from "../src/scripts/components/modal-auth/modal-config.js"
+import {modalSignIn, modalSignUp} from "../src/scripts/components/auth-menu/menu-config.js"
 import {handleSignIn, handleSignUp} from "../src/services/client/auth/auth.js";
+import {buildAuthMenu} from "../src/scripts/components/auth-menu/menu.js";
 
 const user = {
     // name: 'Василий',
     city: 'Москва'
 }
 
-const templates = {
-    'auth_menu': '/src/scripts/components/modal-auth/auth-menu.hbs',
-}
-
 // Ожидаем завершения рендера body, затем строим карточки
 buildBody(user).then(() => {
-    return buildCards().then(() => {
-        let div = document.createElement('div')
-        div.id = 'modals'
+    buildAuthMenu(modalSignUp)
+        .then(() => {
+            document.getElementById(modalSignUp.id).addEventListener('submit', handleSignUp);
+        })
+        .catch(
+            err => {
+                console.log(err);
+            }
+        );
 
-        document.getElementById('main').append(div)
-    });
-}).then(() => {
-    buildModalWithContent(user,
-        'btn__sign_up',
-        templates['auth_menu'],
-        'sign_up__modal',
-        'modal__signup',
-        modalSignUp
-    ).then(() => {
-        document.getElementById(modalSignUp.formId).addEventListener('submit', handleSignUp);
-    }).catch(
-        err => {
-            console.log(err);
-        }
-    );
-    buildModalWithContent(user,
-        'btn__sign_in',
-        templates['auth_menu'],
-        'sign_in__modal',
-        'modal__login',
-        modalSignIn
-    ).then(() => {
-        document.getElementById(modalSignIn.formId).addEventListener('submit', handleSignIn);
-
-    }).catch(
-        err => {
-            console.log(err);
-        }
-    );
+    buildAuthMenu(modalSignIn)
+        .then(() => {
+            document.getElementById(modalSignIn.id).removeEventListener('submit', handleSignIn);
+        })
+        .catch(
+            err => {
+                console.log(err);
+            }
+        );
 });
