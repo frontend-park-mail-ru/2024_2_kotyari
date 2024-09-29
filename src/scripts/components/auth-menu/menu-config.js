87b77ff2ef1c107635cef0ed0882eea839b1f2b8
grozaqueen <yuliyavolkova12@yandex.ru>
2024-09-29 14:17:30 +0300

    [fix] лого изменено

--- src/scripts/components/auth-menu/menu-config.js
+++ src/scripts/components/auth-menu/menu-config.js
@@ -1,6 +1,6 @@
 export const menuSignUp = {
     id: "modal__signup",
-    logo: 'Оксик',
+    logo: 'oxic',
     title: 'Регистрация',
     formId: 'register__form',
     fields: [
@@ -47,7 +47,7 @@ export const menuSignUp = {
 
 export const menuSignIn = {
     id: "modal__login",
-    logo: 'Оксик',
+    logo: 'oxic',
     title: 'Вход',
     formId: 'login__form',
     fields: [
